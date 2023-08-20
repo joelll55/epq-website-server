f9cf3a767b9991b8551f3e9981e599d0fc662773
joelll55 <139919096+joelll55@users.noreply.github.com>
2023-08-20 14:06:50 +0100

    build

--- dist/config.js
+++ dist/config.js
@@ -1,5 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.port = exports.host = void 0;
-exports.host = 'localhost';
-exports.port = 8000;
+exports.host = '35.227.49.115';
+exports.port = 8080;
