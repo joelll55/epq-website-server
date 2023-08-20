0462a8d27f94c1e25f4ed77b95753b198f6c3b71
joelll55 <139919096+joelll55@users.noreply.github.com>
2023-08-20 14:17:47 +0100

    change back to localhost

--- config.ts
+++ config.ts
@@ -1,2 +1,2 @@
-export const host = '35.227.49.115'
+export const host = 'localhost'
 export const port = 8080
--- dist/config.js
+++ dist/config.js
@@ -1,5 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.port = exports.host = void 0;
-exports.host = '35.227.49.115';
+exports.host = '151.229.94.109';
 exports.port = 8080;
